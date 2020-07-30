40a66a854681ce96ca585b9fee20a92d97eb01fd
Ange Ménager <ange-menager@live.fr>
2020-07-30 14:48:25 +0200

    Updated getContact and deleteContact methods to have a more reliable behavior. Turned the console.writeline into Log.information/error

--- MailjetApiClient/MaijetService.cs
+++ MailjetApiClient/MaijetService.cs
@@ -49,7 +49,7 @@ namespace MailjetApiClient
         {
             return _env.IsProduction() || _emulateProduction;
         }
-        public async Task AddContact(bool isExcluded, string contactName, string contactEmail, string contactListID = "") 
+        public async Task<int?> AddContact(bool isExcluded, string contactName, string contactEmail, string contactListID = "") 
         {
             MailjetRequest request = new MailjetRequest
             {
@@ -61,15 +61,15 @@ namespace MailjetApiClient
             MailjetResponse response = await _clientV3.PostAsync(request);
             if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
-                Console.WriteLine(response.GetData());
+                Log.Information($"Total: {response.GetTotal()}, Count: {response.GetCount()}\n");
+                Log.Information(response.GetData().ToString());
+                var responseData = response.GetData();
+                int ID = (int)responseData[0]["ID"];
                 //if the contact is successfully created, push it to a contact list, if it's id is given
                 if (contactListID != "")
                 {
                     try
                     {
-                        var responseData = response.GetData();
-                        int ID = (int)responseData[0]["ID"];
                         MailjetRequest requestToContactList = new MailjetRequest
                         {
                             Resource = ContactManagecontactslists.Resource,
@@ -83,79 +83,99 @@ namespace MailjetApiClient
                         MailjetResponse responseContactList = await _clientV3.PostAsync(requestToContactList);
                         if (response.IsSuccessStatusCode)
                         {
-                            Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", responseContactList.GetTotal(), responseContactList.GetCount()));
-                            Console.WriteLine(responseContactList.GetData());
+                            Log.Information($"Total: {responseContactList.GetTotal()}, Count: {responseContactList.GetCount()}\n");
+                            Log.Information(responseContactList.GetData().ToString());
+                            return ID;
                         }
                         else
                         {
-                            Console.WriteLine(string.Format("StatusCode: {0}\n", responseContactList.StatusCode));
-                            Console.WriteLine(string.Format("ErrorInfo: {0}\n", responseContactList.GetErrorInfo()));
-                            Console.WriteLine(responseContactList.GetData());
-                            Console.WriteLine(string.Format("ErrorMessage: {0}\n", responseContactList.GetErrorMessage()));
+                            Log.Error($"StatusCode: {responseContactList.StatusCode}\n");
+                            Log.Error($"ErrorInfo: {responseContactList.GetErrorInfo()}\n");
+                            Log.Error(response.GetData().ToString());
+                            Log.Error($"ErrorMessage: {responseContactList.GetErrorMessage()}\n");
+                            return ID;
                         }
                     } catch (Exception e)
                     {
                         Log.Error(e.Message);
                         Log.Error(e.StackTrace);
                         Log.Error("InnerException", e.InnerException);
+                        return ID;
                     }
-                }     
+                } else {
+                    return ID;
+                }    
             }
             else
             {
-                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
-                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
-                Console.WriteLine(response.GetData());
-                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+                Log.Error($"StatusCode: {response.StatusCode}\n");
+                Log.Error($"ErrorInfo: {response.GetErrorInfo()}\n");
+                Log.Error(response.GetData().ToString());
+                Log.Error($"ErrorMessage: {response.GetErrorMessage()}\n");
+                return null;
             }
         }
 
-        //TODO add a method to delete a contact when the ContactID is known then upgrade the addcontact method to add custom property then another method to update a contact
+        //TODO upgrade the addcontact method to add custom property then another method to update a contact
         public async Task<int?> GetContactID(string contactEmail)
         {
             MailjetRequest request = new MailjetRequest
             {
-                Resource = Contact.Resource,
-            }.Property(Contact.Email, contactEmail);
+                Resource = new ResourceInfo("contact/"+contactEmail),
+            };
             MailjetResponse response = await _clientV3.GetAsync(request);
             if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
-                Console.WriteLine(response.GetData());
+                Log.Information($"Total: {response.GetTotal()}, Count: {response.GetCount()}\n");
+                Log.Information(response.GetData().ToString());
                 var responseData = response.GetData();
                 int ID = (int)responseData[0]["ID"];
                 return ID;
             }
             else
             {
-                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
-                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
-                Console.WriteLine(response.GetData());
-                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+                Log.Error($"StatusCode: {response.StatusCode}\n");
+                Log.Error($"ErrorInfo: {response.GetErrorInfo()}\n");
+                Log.Error(response.GetData().ToString());
+                Log.Error($"ErrorMessage: {response.GetErrorMessage()}\n");
                 return null;
             }
         }
-
-        public async Task DeleteContact(string contactEmail)
+        
+        // Mailjet doesn't allow deleting a contact with their API, you still need to delete it manually, but at least it won't recieve any mail from this list
+        public async Task<bool> DeleteContactFromContactList(string contactEmail, string contactListID)
         {
-            var ID =  Convert.ToInt64(GetContactID(contactEmail));
-            MailjetRequest request = new MailjetRequest
-            {
-                Resource = Contactdata.Resource,
-                ResourceId = ResourceId.Numeric(ID)
-            };
-            MailjetResponse response = await _clientV3.DeleteAsync(request);
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
-                Console.WriteLine(response.GetData());
-            }
-            else
-            {
-                Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
-                Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
-                Console.WriteLine(response.GetData());
-                Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+            var IDint =  GetContactID(contactEmail);
+            if (IDint != null) {
+                var ID = Convert.ToInt64(IDint.Result);
+                MailjetRequest request = new MailjetRequest
+                {
+                    Resource = ContactManagecontactslists.Resource,
+                    ResourceId = ResourceId.Numeric(ID)
+                }.Property(ContactManagecontactslists.ContactsLists, new JArray {
+                    new JObject {
+                        {"Action", "remove"},
+                        {"ListID", contactListID}
+                    }
+                });
+                MailjetResponse response = await _clientV3.PostAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    Log.Information($"Total: {response.GetTotal()}, Count: {response.GetCount()}\n");
+                    Log.Information(response.GetData().ToString());
+                    return true;
+                }
+                else
+                {
+                    Log.Error($"StatusCode: {response.StatusCode}\n");
+                    Log.Error($"ErrorInfo: {response.GetErrorInfo()}\n");
+                    Log.Error(response.GetData().ToString());
+                    Log.Error($"ErrorMessage: {response.GetErrorMessage()}\n");
+                    return false;
+                } 
+            } else {
+                Log.Error($"ErrorInfo: contact doesn't exist in mailjet\n");
+                return false;
             }
         }
 
